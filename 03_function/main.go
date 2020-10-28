bd6154b58501b07af9e0b641ff0d3c81b1993f18
kunal prakash <kunalprakash1309@gmail.com>
2020-10-28 12:27:22 +0530

    function

---
+++ 03_function/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strings"
+	"text/template"
+)
+
+var tpl *template.Template
+
+type sage struct {
+	Name  string
+	Motto string
+}
+
+type car struct {
+	Manufacturer string
+	Model        string
+	Doors        int
+}
+
+//fm is the function which we pass
+// funcMap is method on template which is type map[string]interfaces{}
+var fm = template.FuncMap{
+	"uc": strings.ToUpper,
+	"ft": firstThree,
+}
+
+func init(){
+	// error by doing this
+	// tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+	// tpl = tpl.Funcs(fm)
+	tpl = template.Must(template.New("").Funcs(fm).ParseFiles("tpl.gohtml"))
+}
+
+func firstThree(s string) string{
+	s = strings.TrimSpace(s)
+	if len(s) >= 3 {
+		s = s[:3]
+	}
+	return s
+}
+
+func main(){
+
+	b := sage{
+		Name:  "Buddha",
+		Motto: "The belief of no beliefs",
+	}
+
+	g := sage{
+		Name:  "Gandhi",
+		Motto: "Be the change",
+	}
+
+	m := sage{
+		Name:  "Martin Luther King",
+		Motto: "Hatred never ceases with hatred but with love alone is healed.",
+	}
+
+	f := car{
+		Manufacturer: "Ford",
+		Model:        "F150",
+		Doors:        2,
+	}
+
+	c := car{
+		Manufacturer: "Toyota",
+		Model:        "Corolla",
+		Doors:        4,
+	}
+
+	sages := []sage{b, g, m}
+	cars := []car{f, c}
+
+	data := struct {
+		Wisdom []sage
+		Transport []car
+	}{
+		sages,
+		cars,
+	}
+
+	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", data)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
