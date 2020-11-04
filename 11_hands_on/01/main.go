a2a1e6db6690c83a160429969fbaa427b12cd16b
kunal prakash <kunalprakash1309@gmail.com>
2020-11-04 12:17:51 +0530

    hands_on

---
+++ 11_hands_on/01/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"html/template"
+)
+
+func foo(w http.ResponseWriter, r *http.Request){
+	io.WriteString(w, "foo ran")
+}
+
+func dog(w http.ResponseWriter, r *http.Request){
+	var tpl *template.Template
+
+	tpl = template.Must(tpl.ParseFiles("dog.gohtml"))
+
+	tpl.ExecuteTemplate(w, "dog.gohtml", nil)
+}
+
+func image(w http.ResponseWriter, r *http.Request){
+	http.ServeFile(w, r, "dog.jpg")
+}
+
+func main(){
+	http.HandleFunc("/", foo)
+	http.HandleFunc("/dog/", dog)
+	http.HandleFunc("/dog.jpg", image)
+	log.Fatalln(http.ListenAndServe(":8080", nil))
+}
---
+++ 11_hands_on/02/main.go
@@ -0,0 +1,12 @@
+// serve all files
+
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main(){
+	log.Fatalln(http.ListenAndServe(":8080", http.FileServer(http.Dir("."))))
+}
